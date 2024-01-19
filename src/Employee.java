be6e4cf9046369a2338805635ab9d3b16e73ba6f
barek2281 <sabdpp17@gmail.com>
2024-01-19 11:42:22 +0600

    some init msg

---
+++ src/Employee.java
@@ -0,0 +1,38 @@
+public class Employee extends Person{
+    private String position;
+    private double salary;
+
+    Employee(){
+        super();
+    }
+    Employee(String name, String surname, String position, double salary){
+        super(name, surname);
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String toString() {
+        return "Employee: " + super.toString();
+    }
+    @Override
+    public double getPaymentAmount() {
+        return salary;
+    }
+
+}
